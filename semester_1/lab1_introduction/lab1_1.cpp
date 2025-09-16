f99891a5fb07908a1cb78e0bc30653e85b7b92a0
nekit.mee <nekit.mee@gmail.com>
2025-09-16 15:20:26 +0300

    Добавил 6 первых заданий из 1 лабораторной работы

---
+++ semester_1/lab1_introduction/lab1_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int a, b, d;
+    std::cout << "Введите переменные А, B, D: \n";
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> d;
+    if (a > b || d < 0) {
+        std::cout << "Ошибка 404, проверьте правильнность ввода:\nb > a\nd > 0";
+        return 404;
+    }
+    else {
+        for (int i=a; i < b;i+=d) {
+            if (i % 3 == 0) {
+                std::cout << i << "\n";
+            }
+        }
+        return 0;
+    }
+}
