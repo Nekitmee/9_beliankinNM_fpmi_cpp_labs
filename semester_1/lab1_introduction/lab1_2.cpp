7a0f30ba55689e42931e14a098b31d677ba84ddc
nekit.mee <nekit.mee@gmail.com>
2025-09-17 08:16:21 +0300

    Lab_1

---
+++ semester_1/lab1_introduction/lab1_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n;
+    int summ = 0;
+    int mult = 1;
+    std::cout << "Введите переменную n:\n";
+    std::cin >> n;
+    if (n <= 1) {
+        std::cout << "Ошибка 404, проверьте условие: \nn > 1";
+        return 404;
+    }
+    else {
+        for (int i = 0; i <= n;) {
+            summ += i;
+            i+=2;
+        }
+        for (int l = 1; l <= n;) {
+            mult *= l;
+            l += 2;
+        }
+        std::cout << summ << "\n";
+        std::cout << mult << "\n";
+        return 0;
+    }
+}
---
+++ semester_1/lab1_introduction/lab1_6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n;
+    int summ = 0;
+    std::cout << "Введите переменную n:  \n";
+    std::cin >> n;
+    std::cout << "\n";
+    if (n <= 0) {
+        std::cout << "Ошибка 404, проверьте правильность ввода: \nn > 0\n";
+        return 404;
+    }
+    else {
+        while (n != 0) {
+            for (int i = 1; n != 0;) {
+                summ += i;
+                i += 2;
+                n -= 1;
+            }
+            std::cout << "Ответ: " << summ;
+        }
+    }
+    return 0;
+}
