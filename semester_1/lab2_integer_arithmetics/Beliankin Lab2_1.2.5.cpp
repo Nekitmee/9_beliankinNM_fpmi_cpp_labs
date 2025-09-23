3e1fa0c56ac2f8a4005de906ebbc7e50acc72b9e
nekit.mee <nekit.mee@gmail.com>
2025-09-23 20:18:16 +0300

    Изменения в lab2

---
+++ semester_1/lab2_integer_arithmetics/Beliankin Lab2_1.2.5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+//Функция: Проверка числа на натуральность 
+bool natural(int a) {
+    if (a < 1) {
+        std::cout << "Ошибка! Число должно быть натуральным.\n";
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+//Функция: Проверка числа на палиндром
+bool palindrome(int b) {
+    int original = b;
+    int rev = 0;
+    while (b > 0) {
+        rev = rev * 10 + b % 10;
+        b /= 10;
+    }
+    return (original == rev);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n;
+    std::cout << "Введите натуральное число: ";
+
+    //Проверка ввода числа
+    if (!(std::cin >> n)) {
+        std::cout << "Ошибка: введено не число.\n";
+        return 1;
+    }
+    //Проверка числа на натуральность 
+    if (!natural(n)) {
+        return 1;
+    }
+    //Проверка числа и его квадрата на палиндром
+    if (palindrome(n) && palindrome(n * n)) {
+        std::cout << "Ваше число является двойным палиндромом!\n";
+    }
+    else {
+        std::cout << "Ваше число не является двойным палиндромом.\n";
+    }
+    return 0;
+}
---
+++ semester_1/lab2_integer_arithmetics/Beliankin Lab2_1.3.1.cpp
@@ -0,0 +1,31 @@
+﻿/*Нужно ввести число -2147483648 так как для типа int абсолютные показатели
+наибольшего и наименьшего возможного значения будут отличаться на единицу
+(минимальное - 2147483648, максимальное 2147483647), из-за этого функция
+abs() не сможет найти такое же положительное число и примет значение -2147483648,
+а дальше функция IsNotNegative() примет значение true, а функция std_non_negative_check()
+false и вылезет сообщение "Shit, you broke my program :("*/
+
+#include <iostream>
+void TryRead(int& number) {
+	if (!(std::cin >> number)) {
+		std::cout << "Fail on reading the number." << std::endl;
+		exit(0);
+	}
+}
+bool IsNotNegative(int number) {
+	return abs(number) == number;
+}
+int main() {
+	int number;
+	std::cout << "Enter the number: ";
+	TryRead(number);
+	bool std_non_negative_check = (number >= 0);
+	if (IsNotNegative(number) == std_non_negative_check) {
+		std::cout << "Try again...." << std::endl;
+	}
+	else {
+		std::cout << "Shit, you broke my program :(" << std::endl;
+		exit(1);
+	}
+	return 0;
+}
